b957013183e290f5969e34020607dcebeec66b3b
Leandro <leanf94@gmail.com>
2023-11-25 18:35:30 -0300

    add-todos

--- src/VotingApp.jsx
+++ src/VotingApp.jsx
@@ -2,10 +2,8 @@ import { BrowserRouter } from "react-router-dom"
 import { AppRouter } from "./router"
 
 //TODO
-// Hacer en el navbar el loguito para que se loguee, lo tenes en VitaNav copialo
 // Crear los usuarios y el token JWT, seria con el store?
-// Eliminar un juego ---> El id va por el body del JSON. No por url creo oka
-// CREATE del juego
+// Hacer el post para que un juez pueda realizar una votación
 
 export const VotingApp = () => {
     return (
