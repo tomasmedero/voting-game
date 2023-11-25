27eaea5020c56124128f551c204aaf47a36ce3c1
Leandro <leanf94@gmail.com>
2023-11-25 18:33:50 -0300

    add-dropdown-navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,33 @@
+import { useEffect, useRef, useState } from "react"
 import { NavLink } from "react-router-dom"
 
 export const NavBar = () => {
+    const [isOpenProfile, setIsOpenProfile] = useState(false)
+    const dropdownRef = useRef(null)
+
+    const toggleDropdownProfile = () => {
+        setIsOpenProfile(!isOpenProfile)
+    }
+    const closeDropdownProfile = () => {
+        setIsOpenProfile(false)
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target)
+            ) {
+                closeDropdownProfile()
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside)
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, [])
     return (
         <nav className="flex justify-between px-10 py-3 font-bold bg-black text-white">
             <div className="font-bold">
@@ -31,6 +58,41 @@ export const NavBar = () => {
                         Jueces
                     </NavLink>
                 </li>
+                <div
+                    className="relative inline-block text-left"
+                    ref={dropdownRef}
+                >
+                    <div>
+                        <button
+                            onClick={toggleDropdownProfile}
+                            className=" flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300"
+                        >
+                            <img
+                                className="w-8 h-8 rounded-full"
+                                src="/default-avatar.png"
+                                alt="Avatar"
+                            />
+                        </button>
+                    </div>
+
+                    {isOpenProfile && (
+                        <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                            <div
+                                className="py-1"
+                                role="menu"
+                                aria-orientation="vertical"
+                                aria-labelledby="options-menu"
+                            >
+                                <a
+                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                                    role="menuitem"
+                                >
+                                    Salir
+                                </a>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </ul>
         </nav>
     )
